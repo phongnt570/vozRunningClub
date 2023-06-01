5e81adfa1f3adc67d1c422e8851000d949336a7f
Tuan-Phong Nguyen <tuanphong94@gmail.com>
2023-06-01 23:17:16 +0200

    Update links in navbar

--- weeklyTracking/urls.py
+++ weeklyTracking/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("leaderboard/", views.index, name="leaderboard"),
     path("top-donates/", views.top_donates, name="top-donates"),
     path("about/", views.about, name="about"),
 
