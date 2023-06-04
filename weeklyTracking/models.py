8cd21ddf68fd4c8a84343ea6062a84985e3f0ba6
Tuan-Phong Nguyen <tuanphong94@gmail.com>
2023-06-04 21:21:15 +0200

    null voz_name -> empty string

--- weeklyTracking/models.py
+++ weeklyTracking/models.py
@@ -7,7 +7,7 @@ class StravaRunner(models.Model):
     strava_id = models.IntegerField(primary_key=True)
     strava_name = models.CharField(max_length=64)
     strava_refresh_token = models.CharField(max_length=64, null=True, blank=True)
-    voz_name = models.CharField(max_length=64, blank=True, null=True)
+    voz_name = models.CharField(max_length=64, blank=True, default="")
 
     last_updated = models.DateTimeField(auto_now=True)
 
