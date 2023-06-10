5e5da989f9e0a59d4e93f32865d3f3ba65844b99
Tuan-Phong Nguyen <tuanphong94@gmail.com>
2023-06-10 22:25:54 +0200

    more logging

--- weeklyTracking/middleware.py
+++ weeklyTracking/middleware.py
@@ -1,7 +1,7 @@
 from django.contrib import messages
 from django.shortcuts import redirect
 from django.urls import reverse
-from social_core.exceptions import AuthAlreadyAssociated
+from social_core.exceptions import AuthAlreadyAssociated, AuthCanceled
 from social_django.middleware import SocialAuthExceptionMiddleware
 
 
@@ -9,7 +9,13 @@ class StravaAuthAlreadyAssociatedMiddleware(SocialAuthExceptionMiddleware):
     """Redirect users to Profile page when AuthAlreadyAssociated exception occurs."""
 
     def process_exception(self, request, exception):
-        if isinstance(exception, AuthAlreadyAssociated):
-            if request.backend.name == "strava":
+        if request.backend.name == "strava":
+            if isinstance(exception, AuthAlreadyAssociated):
                 messages.error(request, "Strava account already associated with another account.")
                 return redirect(reverse("profile"))
+            if isinstance(exception, AuthCanceled):
+                messages.error(request, "Strava authentication canceled.")
+                return redirect(reverse("registration"))
+            else:
+                messages.error(request, "Strava authentication failed.")
+                return redirect(reverse("registration"))
