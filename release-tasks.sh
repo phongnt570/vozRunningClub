a2576a06644439fa3b8899c717d3cdec1e67de12
Tuan-Phong Nguyen <tuanphong94@gmail.com>
2023-06-11 19:45:13 +0200

    add time_aware

--- release-tasks.sh
+++ release-tasks.sh
@@ -10,6 +10,6 @@ echo "Running add_default_settings"
 python manage.py add_default_settings
 
 echo "Running update_leaderboard"
-python manage.py update_leaderboard
+python manage.py update_leaderboard --time_aware
 
 echo "Done running release tasks"
