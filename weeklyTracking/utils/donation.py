ace423d0c328877f0d77a99602a3739362ddd367
Tuan-Phong Nguyen <tuanphong94@gmail.com>
2023-06-11 19:51:13 +0200

    fix update donations

--- weeklyTracking/utils/donation.py
+++ weeklyTracking/utils/donation.py
@@ -1,4 +1,5 @@
-from weeklyTracking.models import WeeklyProgress, SettingWeekBaseDonation, SettingDefaultWeekBaseDonation
+from weeklyTracking.models import WeeklyProgress, SettingWeekBaseDonation, SettingDefaultWeekBaseDonation, \
+    SettingDefaultDonationByWeek, SettingDefaultDonation
 
 
 def get_or_create_week_base_donation(year: int, week_num: int, distance: int) -> SettingWeekBaseDonation:
@@ -10,6 +11,15 @@ def get_or_create_week_base_donation(year: int, week_num: int, distance: int) ->
                                                       base_donation=default_setting.base_donation)
 
 
+def get_or_create_default_donation_by_week(year: int, week_num: int) -> SettingDefaultDonationByWeek:
+    try:
+        return SettingDefaultDonationByWeek.objects.get(year=year, week_num=week_num)
+    except SettingDefaultDonationByWeek.DoesNotExist:
+        default_donation = SettingDefaultDonation.objects.get()
+        return SettingDefaultDonationByWeek.objects.create(year=year, week_num=week_num,
+                                                           default_donation=default_donation.default_donation)
+
+
 def update_donation(weekly_progress: WeeklyProgress):
     reg_dis = weekly_progress.registered_mileage.distance
 
@@ -17,8 +27,9 @@ def update_donation(weekly_progress: WeeklyProgress):
                                                distance=reg_dis)
     donation_per_km = setting.base_donation
 
-    if reg_dis > 0 and weekly_progress.distance == 0:
-        donation = weekly_progress.registered_mileage.distance * donation_per_km
+    if reg_dis > 0 and weekly_progress.distance == 0:  # if user registered but has not run any km
+        donation = get_or_create_default_donation_by_week(year=weekly_progress.year,
+                                                          week_num=weekly_progress.week_num).default_donation
     else:
         donation = donation_per_km * (weekly_progress.registered_mileage.distance - weekly_progress.distance)
 
